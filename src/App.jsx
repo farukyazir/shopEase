cc338d6cbba6afe8b6a526927f814e9ff0f6d1cf
Faruk Yazir <farukyazir04@gmail.com>
2026-09-24 09:15:18 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Banner from './components/Banner'
 import ProductCard from './components/ProductCard'
 import products from './data/productData'
 import "./App.css";
-import Header from './components/Header';
+import Header from './components/Header'
 const App = () => {
   const[cartCount,setcartCount]= useState(0);
 
